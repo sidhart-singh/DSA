b4047eac2115783d52937955fdc94fc2e9df0a2e
Sidhart Singh <isidhartsingh@gmail.com>
2023-01-02 13:55:31 +0530

    added isBalanced(Node root) in isBalanced.java

--- BinaryTree/Main.java
+++ BinaryTree/Main.java
@@ -1,11 +1,15 @@
 package BinaryTree;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
         BinaryTree bt = new BinaryTree();
         Node root = bt.createTree();
+        isBalanced isBalanced = new isBalanced();
 
-        System.out.println("Count of Good Nodes: " + CountGoodNodes.countGoodNode(root, root.data));;
+        // System.out.println("Count of Good Nodes: " + CountGoodNodes.countGoodNode(root, root.data));;
+        System.out.println("isBalanced: " + Arrays.toString(isBalanced.isBalancedUtil(root)));
     }
 }
---
+++ BinaryTree/isBalanced.java
@@ -0,0 +1,33 @@
+package BinaryTree;
+
+public class isBalanced {
+    public int[] isBalancedUtil(Node root){
+        // [ isBalanced, height ]
+        
+        // PostOrder Traversal: Left -> Right -> Node
+        
+        // BaseCondition
+        if(root == null)
+            // { isBalanced : true, height : 0 }
+            return new int[]{1, 0};
+        
+        // Left
+        int[] left = isBalancedUtil(root.left);
+        
+        // Right
+        int[] right = isBalancedUtil(root.right);
+        
+        // Node
+        int isBalanced = 0;
+        if(
+            // left and right subtree are balanced
+            left[0] == 1 && right[0] == 1 && 
+            
+            // root is balanced
+            Math.abs(left[1] - right[1]) <= 1)
+        isBalanced = 1;
+            
+        // { isBalanced : true, height : 0 }
+        return new int[]{ isBalanced, Math.max(left[1], right[1]) + 1};
+    }
+}
