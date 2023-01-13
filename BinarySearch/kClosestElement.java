6e30acc6839c73b7d7bede1c904b89d0c20ec0de
Sidhart Singh <isidhartsingh@gmail.com>
2023-01-13 17:01:09 +0530

    added kClosestElement.java

---
+++ BinarySearch/kClosestElement.java
@@ -0,0 +1,30 @@
+package BinarySearch;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class kClosestElement {
+
+    int nums[] = {1,2,3,4,5};
+    int k = 4;
+    int x = -1;
+    public List<Integer> findClosestElements() {
+        int low = 0;
+        int high = nums.length - k;
+
+        while(low < high){
+            int mid = low + ((high - low) / 2);
+
+            if(x - nums[mid] > nums[mid + k] - x){
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        int[] result = Arrays.copyOfRange(nums, low, low + k);
+        return Arrays.stream(result)        // IntStream
+                    .boxed()          // Stream<Integer>
+                    .collect(Collectors.toList());
+    }
+}
