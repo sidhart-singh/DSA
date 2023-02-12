e0f4d2a0292792505b424cfdf552fd87a69c3ece
Sidhart Singh <isidhartsingh@gmail.com>
2023-02-12 13:05:39 +0530

    added NamingACompany.java and main()

--- Arrays/Main.java
+++ Arrays/Main.java
@@ -1,5 +1,6 @@
 package Arrays;
 
+import java.rmi.Naming;
 import java.util.Arrays;
 
 import BinarySearch.kClosestElement;
@@ -21,13 +22,27 @@ public class Main {
         // FlipStringToMonotoneIncreasing fstmi = new FlipStringToMonotoneIncreasing();
         // System.out.println(fstmi.minFlipsMonoIncr());
 
+<<<<<<< HEAD
         LongestConsecutiveSequence lcs = new LongestConsecutiveSequence();
         System.out.println(lcs.longestConsecutive());
+=======
+        // LongestConsecutiveSequence lcs = new LongestConsecutiveSequence();
+        // System.out.println(lcs.longestConsecutive());
+>>>>>>> aa1723e (added NamingACompany.java and main())
 
         // NextGreaterElementII ngeii = new NextGreaterElementII();
         // System.out.println(Arrays.toString(ngeii.nextGreaterElements()));
 
         // KFrequentElements kfe = new KFrequentElements();
         // System.out.println(Arrays.toString(kfe.topKFrequentII()));
+<<<<<<< HEAD
+=======
+
+        // FirstMissingPositive fmp = new FirstMissingPositive();
+        // System.out.println(fmp.firstMissingPositive(new int[]{1}));
+
+        NamingACompany nac = new NamingACompany();
+        System.out.println(nac.distinctNames(new String[]{"coffee","donuts","time","toffee"}));;
+>>>>>>> aa1723e (added NamingACompany.java and main())
     }
 }
---
+++ Arrays/NamingACompany.java
@@ -0,0 +1,42 @@
+package Arrays;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class NamingACompany {
+    public long distinctNames(String[] ideas) {
+        
+        HashMap<Character, HashSet<String>> wordMap = new HashMap<>();
+        long result = 0;
+
+        for(String s : ideas){
+            if(wordMap.get(s.charAt(0)) == null)
+                wordMap.put(s.charAt(0), new HashSet<>());
+            wordMap.get(s.charAt(0)).add(s.substring(1));
+        }
+
+        for(Map.Entry<Character, HashSet<String>> e: wordMap.entrySet())
+            System.out.println(e);
+
+        for(Map.Entry<Character, HashSet<String>> e1 : wordMap.entrySet()){
+            for(Map.Entry<Character, HashSet<String>> e2 : wordMap.entrySet()){
+                if(e1.getKey() == e2.getKey())
+                    continue;
+                
+                int intersect = 0;
+                for(String word : e1.getValue()){
+                    if(e2.getValue().contains(word))
+                        intersect++;
+                }
+
+                int distinct1 = e1.getValue().size() - intersect;
+                int distinct2 = e2.getValue().size() - intersect;
+
+                result += distinct1 * distinct2;
+            }
+        }
+
+        return result;
+    }
+}
